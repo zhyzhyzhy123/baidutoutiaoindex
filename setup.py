21c32c0847a234cedcc946b9dc9e5cbe7944bc9a
zhyzhyzhy123 <31156721+zhyzhyzhy123@users.noreply.github.com>
2021-01-05 15:45:49 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'bdindex',
+    version = '0.1.1',
+    keywords='spider baidu toutiao',
+    description = 'A spider on baidu and toutiao',
+    license = 'MIT License',
+    url = 'https://github.com/zhyzhyzhy123/baidutoutiaoindex',
+    author = 'Hongyi Zhu',
+    author_email = '472739561@qq.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
